b4b85ba392187997b311fbd0950e7a263a510398
unknown <andybabenko4@gmail.com>
2023-09-24 11:23:07 +0300

    Fix sendMessage for check all date input

--- sendMessages.js
+++ sendMessages.js
@@ -23,24 +23,54 @@ const updateFlags = (filePath, flags) => {
 
 
 const sendMessages = (bot, chatId, sortedArray) => {
-   // Load previous flags or initialize an empty object
-  let flags = loadFlags('flags.json');
+ // Load previous flags or initialize an empty object
+let flags = loadFlags('flags.json');
 
-  // Update the flags and save them
-  sortedArray.forEach(element => {
-    if (!element.sent) {
-      bot.sendMessage(chatId, `${element.name} \n${element.date} \n${element.data}`);
-      element.sent = true;  // Update the sent flag
-      flags[element.name] = { sent: true, read: false };  // Update both flags
+// Update sortedArray with flags from flags.json
+sortedArray.forEach(element => {
+  const sensorName = element.name;
+
+  // Check if the sensor has flags
+  if (flags[sensorName]) {
+    const dates = Object.keys(flags[sensorName]);
+
+    // Update flags for each date in sortedArray
+    dates.forEach(date => {
+      const foundDateIndex = sortedArray.findIndex(item => item.name === sensorName && item.date === date);
+
+      if (foundDateIndex !== -1) {
+        sortedArray[foundDateIndex].sent = flags[sensorName][date].sent;
+        sortedArray[foundDateIndex].read = flags[sensorName][date].read;
+      }
+    });
+  }
+});
+
+// Update the flags and save them
+sortedArray.forEach(element => {
+  const sensorName = element.name;
+
+  if (!element.sent) {
+    bot.sendMessage(chatId, `${element.name} \n${element.date} \n${element.data}`);
+    element.sent = true;  // Update the sent flag
+
+    // Create flags for each date if not already present
+    if (!flags[sensorName]) {
+      flags[sensorName] = {};
     }
+
+    // Update the sent flag for this date
+    flags[sensorName][element.date] = { sent: true, read: false };
+  }
 
   // Mark as read
   element.read = true;
-  flags[element.name].read = true;
+  flags[sensorName][element.date].read = true;
 });
 
 // Save the updated flags
 updateFlags('flags.json', flags);
+
 }
 
   
