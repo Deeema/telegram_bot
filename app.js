5d61e7434fc80d38263083183ab6521b5d8b7cff
Lee <dima.lee@gmail.com>
2023-10-18 16:27:45 +0300

    Changes for DB query

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const insertEventRecord = (messageId, censorId, censor_time, successful, message
   });
 };
 
-const sendColumnValuesToTelegram = async (tableName, excludedColumns, rowId) => {
+const sendColumnValuesToTelegram = async (tableName, excludedColumns, rowId, callback) => {
   try {
     const columnNames = await getColumnNames(tableName);
 
@@ -26,7 +26,14 @@ const sendColumnValuesToTelegram = async (tableName, excludedColumns, rowId) => 
 
     const includedColumns = columnNames.filter(column => !excludedColumns.includes(column));
 
-    const query = `SELECT ROWID, ${includedColumns.join(', ')} FROM ${tableName} WHERE ROWID > (SELECT MAX(ROWID) FROM events WHERE message_sent = 1)`;
+    // const query = `SELECT ROWID, ${includedColumns.join(', ')} FROM ${tableName} WHERE ROWID > (SELECT MAX(ROWID) FROM events WHERE message_sent = 0)`;
+    const maxMessageIdQuery = 'SELECT MAX(message_id) AS max_message_id FROM events WHERE message_sent = 0';
+
+    const query = `
+      SELECT ROWID, ${includedColumns.join(', ')}
+      FROM ${tableName}
+      WHERE ROWID > 5406
+    `;
 
     db.get(query, [], async (err, row) => {
       if (err) {
@@ -36,8 +43,10 @@ const sendColumnValuesToTelegram = async (tableName, excludedColumns, rowId) => 
         await sendTelegramMessage(message);
 
         console.log('Message sent successfully.');
+        // Call the callback after sending the message
+        callback(rowId);
       } else {
-        console.log('No new rows to send.');
+        console.log('No new rows to send.', row);
       }
     });
   } catch (error) {
@@ -49,18 +58,45 @@ const sendColumnValuesToTelegram = async (tableName, excludedColumns, rowId) => 
 const tableName = 'TableDataGRP';
 const excludedColumns = ['BitSetup', 'FlP_1', 'FlP_2'];  // Specify the columns to be excluded
 
-// Setup the event watcher for new censor records
 watchForNewCensorRecords((newCensorRecords) => {
   newCensorRecords.forEach(censorRecord => {
-    const parent_id = censorRecord.TabNo;
+    const rowId = censorRecord.TabNo;  // Use a single variable for the rowId
     const censorId = censorRecord.Identifier;
-    const censor_time = censorRecord.TimerOut;
+    const censorTime = censorRecord.TimerOut;
     const successful = true;
-    const message_sent = 1;
+    const messageSent = 0;
 
-    if (newCensorRecords.length <= parent_id) {
-      sendColumnValuesToTelegram(tableName, excludedColumns, rowId);
-      insertEventRecord(parent_id, censorId, censor_time, successful, message_sent);
-    }
+    const eventExistsQuery = 'SELECT COUNT(*) AS count FROM Events WHERE message_id = ?';
+    db.get(eventExistsQuery, [rowId], (err, result) => {
+      if (err) {
+        console.error('Error checking for existing event:', err.message);
+        return;
+      }
+
+      const eventExists = result.count > 0;
+      console.log("eventExists", eventExists, rowId);
+
+      if (!eventExists) {
+        insertEventRecord(rowId, censorId, censorTime, successful, messageSent);
+        console.log(`Event with rowId ${rowId} not exists.`);
+        sendColumnValuesToTelegram(tableName, excludedColumns, rowId, () => {
+          updateEventMessageSentFlag(rowId);
+        });
+      } else {
+        // console.log(`Event with rowId ${rowId} already exists. Skipping message sending.`);
+      }
+    });
   });
 });
+
+const updateEventMessageSentFlag = (parentId) => {
+  console.log("======", parentId);
+  const updateQuery = 'UPDATE Events SET message_sent = 1 WHERE message_id = ?';
+  db.run(updateQuery, [parentId], function (err) {
+    if (err) {
+      console.error('Error updating event record:', err.message);
+    } else {
+      console.log('Updated event record with ID:', parentId, 'message_sent set to 1');
+    }
+  });
+};
